190f8f5e5f45742da4892b1cbac29ebac3b928ad
markrizkalla <mark.rizkalla1@gmail.com>
2023-03-10 08:47:03 -0800

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,11 +8,19 @@
  * Return: 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	printf_s("%d\n", argc);
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
 
 	return (0);
 }
