1748a50bdd6e3ae2dcaef8eec6ef3d2679061ade
markrizkalla <mark.rizkalla1@gmail.com>
2024-02-06 08:42:48 +0200

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,6 +17,9 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
+	if (array == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
